355d0c13717c9f4c7e1daa649413fbcb15944e92
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-09-19 11:54:59 -0700

    Adding role::osx_workstation and role::puppet_server

---
+++ manifests/osx_workstation.pp
@@ -0,0 +1,10 @@
+# @summary A role class to setup an osx workstation
+#
+# This includes the profiles need to setup a osx workstation
+#
+# @example
+#   include role::osx_workstation
+class role::osx_workstation {
+    include profile::base
+    
+}
