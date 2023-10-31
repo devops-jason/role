fe2262ad567ecf4b1ddad62d10d5fb52f563c8fb
Jason Lee Miller <jasonlmiller@jlmillerelectric.com>
2023-10-31 13:28:33 -0700

    Adding hiera lookup to be safe

--- manifests/puppet_server.pp
+++ manifests/puppet_server.pp
@@ -5,8 +5,11 @@
 # @example
 #   include role::puppet_server
 class role::puppet_server {
-    include profile::base
-    include profile::linux::base
-    include profile::puppet::puppet_server
-    
+  include profile::base
+  include profile::linux::base
+  class { 'profile::puppet::puppet_server':
+    min_heap_size     => lookup('puppetserver::min_heap_size', '512m'),
+    max_heap_size     => lookup('puppetserver::max_heap_size', '512m'),
+    r10k_control_repo => lookup('puppetserver::r10k_control_repo', '')
+  }
 }
